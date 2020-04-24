2d0b6bf7ac11c397ac3da24f9c1e4d9914ca690b
Pete Parks <codein@mac.com>
2020-04-23 21:40:09 -0700

    Done with get getting the multi-section table view working with CoreSata with filterd sortng

---
+++ BrunchHype/BrunchHype/View Controllers/Custom Views/BrunchSpotListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BrunchSpotListTableViewCell.swift
+//  BrunchHype
+//
+//  Created by Pete Parks on 4/23/20.
+//  Copyright © 2020 Karl Pfister. All rights reserved.
+//
+
+import UIKit
+
+class BrunchSpotListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
